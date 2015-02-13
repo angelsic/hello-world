ed96e262d8893aaa409d0d2954b7888be7eaf166
angelsic <newangeled@gmail.com>
2015-02-12 21:07:07 -0600

    Create a Helloworld.java
    clase principal de Helloworld

---
+++ Helloworld/src/com/linux/helloworld/Helloworld.java
@@ -0,0 +1,10 @@
+package com.linux.helloworld;
+
+public class Helloworld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hola MUndo!!!");
+	}
+
+}
